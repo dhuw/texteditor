2a19848d218d22dd182fd1ef40d5a3ea56587aee
dhuw <hazlewood.dyl@gmail.com>
2022-09-23 23:24:38 -0700

    install.js update

---
+++ client/src/js/install.js
@@ -0,0 +1,24 @@
+const butInstall = document.getElementById('buttonInstall');
+
+window.addEventListener('beforeinstallprompt', (event) => {
+    window.defferedPrompt = event;
+    butInstall.classList.toggle('hidden', false);
+});
+
+
+butInstall.addEventListener('click', async () => {
+    const promptEvent = window.defferedPrompt;
+
+    if (!promptEvent) {
+        return;
+    }
+
+    promptEvent.prompt();
+    window.defferedPrompt = null;
+    butInstall.classList.toggle('hidden', true);
+});
+
+
+window.addEventListener('appinstalled', (event) => {
+    window.defferedPrompt = null;
+});
